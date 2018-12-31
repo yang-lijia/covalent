140835311b6c715e8db043d0d5b3bae7d848b7a5
Robin Cher <robin_cher@tech.gov.sg>
2018-12-31 15:34:55 +0800

    Remove generated JS files

--- src/modules/command/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var command_manager_1 = require("./command.manager");
-exports.CommandManager = command_manager_1["default"];
-var command_processor_1 = require("./command.processor");
-exports.CommandProcessor = command_processor_1["default"];
--- src/modules/talent/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var happytalent_1 = require("./happytalent");
-exports.HappyTalent = happytalent_1["default"];
-var helptalent_1 = require("./helptalent");
-exports.HelpTalent = helptalent_1["default"];
