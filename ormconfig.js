ec4ddddd7e2e74aefc9bf7bd71156bbd5f5808a9
Wei Junyu <junyuwei@gmail.com>
2019-03-23 16:41:58 +0800

    Formatting config files

--- ormconfig.js
+++ ormconfig.js
@@ -1,19 +1,19 @@
 const path = require('path');
 
 module.exports = {
-   "type": "sqlite",
-   "database": `${path.resolve(__dirname, "data/covalent.db")}`,
-   "synchronize": process.env.NODE_ENV !== 'production',
-   "migrationsRun": false,
-   "logging": true,
-   "entities": [
-      "src/entity/**/*.ts"
-   ],
-   "migrations": [
-      "dist/migration/**/*.js"
-   ],
-   "cli": {
-      "entitiesDir": "src/entity",
-      "migrationsDir": "src/migration"
-   }
+    "type": "sqlite",
+    "database": `${path.resolve(__dirname, "data/covalent.db")}`,
+    "synchronize": process.env.NODE_ENV !== 'production',
+    "migrationsRun": false,
+    "logging": true,
+    "entities": [
+        "src/entity/**/*.ts"
+    ],
+    "migrations": [
+        "dist/migration/**/*.js"
+    ],
+    "cli": {
+        "entitiesDir": "src/entity",
+        "migrationsDir": "src/migration"
+    }
 }
